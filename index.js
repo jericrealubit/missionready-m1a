74069c2fa5bca963448449964b3a3123717d82c0
jeric realubit <jericrealubit@gmail.com>
2023-12-21 03:56:19 +1300

    6

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
+// import express from "express";
+
+// const app = express();
+// const PORT = process.env.PORT || 8080;
+// const router = express.Router();
+
+// router.get("/", (req, res) => {
+//   res.send("xxx");
+// });
+
+// app.listen(PORT, () =>
+//   console.log(`Server running on port: http://localhost:${PORT}`)
+// );
+
 import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
+const router = express.Router();
+
+app.use(cors());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
 
 app.listen(PORT, () =>
   console.log(`Server running on port: http://localhost:${PORT}`)
@@ -10,3 +35,5 @@ app.listen(PORT, () =>
 router.get("/", (req, res) => {
   res.send("xxx");
 });
+
+app.use("/", router);
